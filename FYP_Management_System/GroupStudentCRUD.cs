324ee2e1671911f1f50989b6a076581e490e7907
Saad Ahmad Malik <saadislion@gmail.com>
2024-03-21 22:20:48 +0500

    ******

--- FYP_Management_System/GroupStudentCRUD.cs
+++ FYP_Management_System/GroupStudentCRUD.cs
@@ -16,5 +16,10 @@ namespace FYP_Management_System
         {
             InitializeComponent();
         }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
