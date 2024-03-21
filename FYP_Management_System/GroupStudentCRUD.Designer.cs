6cdb81d0a7f459405e3ced9130b91f14e359d769
Saad Ahmad Malik <saadislion@gmail.com>
2024-03-21 21:25:47 +0500

    ******

---
+++ FYP_Management_System/GroupStudentCRUD.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace FYP_Management_System
+{
+    partial class GroupStudentCRUD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // GroupStudentCRUD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1265, 638);
+            this.Name = "GroupStudentCRUD";
+            this.Text = "GroupStudentCRUD";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
